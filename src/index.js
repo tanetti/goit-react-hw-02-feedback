ec495df7d903f7f76f1f2cca458be7ff0bc7f3ff
Volodymyr Dudka <tanetti22@gmail.com>
2022-08-08 20:41:26 +0300

    Add base settings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'constants/theme';
 
