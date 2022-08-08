7a8d64e9d3c1ae11d18f4bb43f5165d4fc0b2a32
Volodymyr Dudka <tanetti22@gmail.com>
2022-08-08 20:35:24 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { GlobalStyles } from './GlobalStyles';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React template
+      <GlobalStyles />
+    </div>
+  );
+};
